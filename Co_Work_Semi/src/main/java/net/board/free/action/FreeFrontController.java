74e3fd707614aa4d30e1c49b2dcac54cd2eeefd4
qwe79 <qwe79@DESKTOP-9KI5UIK>
2023-02-01 17:36:59 +0900

    test

--- Co_Work_Semi/src/main/java/net/board/free/action/FreeFrontController.java
+++ Co_Work_Semi/src/main/java/net/board/free/action/FreeFrontController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ProjectFrontController
  */
-@WebServlet("*.net")
+@WebServlet("*.bo")
 public class FreeFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -40,7 +40,7 @@ public class FreeFrontController extends HttpServlet {
 		Action action = null;
 		
 		switch(command) {
-		case "/logo.net" :
+		case "/logo.bo" :
 			action = new FreeLogoAction();
 			break;
 		}
