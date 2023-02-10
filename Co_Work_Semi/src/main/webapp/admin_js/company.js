e87e7041569b5b770bb758b32e43dfa76d6b8d39
Deokgyun an <97350241+deokgyun@users.noreply.github.com>
2023-02-10 17:52:59 +0900

    0210 회사정보, 구성원관리 작업

--- Co_Work_Semi/src/main/webapp/admin_js/company.js
+++ Co_Work_Semi/src/main/webapp/admin_js/company.js
@@ -8,22 +8,35 @@ $(function() {
 		var val = $(this).parent().prev().children().first().val();
 		var val2 = $(this).parent().prev().children().eq(1).val();
 		var val3 = $(this).parent().prev().children().eq(2).val();
-			if ($(this).text() == "수정") {
+
+		if ($(this).text() == "수정") {
 			$(this).parent().prev().children().first().attr("readOnly", false)
 			$(this).parent().prev().children().first().attr("disabled", false)
 			$(this).parent().prev().children().first().focus()
 			$(this).parent().prev().children().first().val('')
 			$(this).parent().prev().children().first().val(val)
+			$("button[type=submit]").not(this).hide();
+			$("button[name=del]").hide();
+			$(".bootstrap-touchspin-up").not(this).hide();
 			$(this).text('확인')
 			$(this).next().hide();
 			$(this).next().next().show();
 
 		} else if ($(this).text() == "확인") {
-			$(this).unbind('click').click()
+			var answer = confirm(val + " 변경 하시겠어요?");
+			if (answer) {
+				// 수정 완료 시
+				$(this).unbind('click').click()
+			} else {
+				$(this).parent().prev().children().first().focus()
+				$(this).parent().prev().children().first().val('')
+				$(this).parent().prev().children().first().val(val)
+			}
 		}
 		e.stopPropagation();
 	}) // 수정 버튼 클릭
 	// + 버튼 클릭
+
 	$(".bootstrap-touchspin-up").on("click", function() {
 		$(this).hide();
 		$(this).next().show();
@@ -35,7 +48,9 @@ $(function() {
 		$(this).parent().prev().children().first().attr("disabled", true)
 		$(this).prev().prev().text('수정')
 		$(this).hide()
-		$(this).prev().show();
+		$("button[type=submit]").show();
+		$("button[name=del]").show();
+		$("button").show();
 	})
 
 	$("#second").click(function() {
@@ -44,6 +59,18 @@ $(function() {
 		$(this).prev().prev().text('수정')
 		$(this).hide()
 		$(this).prev().show();
+	})
+
+	$(document).on("click", "button[name=del]", function() {
+		var del = $(this).parent().prev().children().first().val();
+		var answer = confirm(del + "  삭제하시겠습니까?");
+		if (answer) {
+			$(this).unbind('click').click()
+		} else {
+			$(this).parent().prev().children().first().focus()
+			$(this).parent().prev().children().first().val('')
+			$(this).parent().prev().children().first().val(val)
+		}
 	})
 
 
